b6d58b9128febe44c4d7445e9c83336b8242197f
stephen <scdodd83@gmail.com>
2024-11-15 17:48:17 -0600

    adding UI tests

---
+++ tests/test_login.py
@@ -0,0 +1,25 @@
+import re
+from playwright.sync_api import Page, expect
+
+def test_has_title(page: Page):
+    page.goto("https://www.saucedemo.com/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Swag Labs"))
+
+def test_login(page: Page):
+    page.goto("https://www.saucedemo.com/")
+
+    # Enter username.
+    page.get_by_placeholder("Username").click()
+    page.get_by_placeholder("Username").fill("standard_user")
+
+    # Enter password.
+    page.get_by_placeholder("Password").click()
+    page.get_by_placeholder("Password").fill("secret_sauce")
+
+    # Click login button.
+    page.locator("#login-button").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.locator(".app_logo")).to_be_visible()
